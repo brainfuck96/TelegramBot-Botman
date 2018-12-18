dbb6796ffca557603184377580a3761426fa4b55
Dmitry Plohotnyuk <dmitryplohotnyuk@MacBook-Pro-Dmitry.local>
2018-12-18 21:39:52 +0200

    Added Bot\CinemaController

--- app/Http/Controllers/Bot/StartController.php
+++ app/Http/Controllers/Bot/StartController.php
@@ -7,12 +7,26 @@ use App\Http\Controllers\Controller;
 
 class StartController extends Controller
 {
-    public function index($bot)
+    public function web($bot)
     {
         $bot->reply('Список доступных команд:');
         $bot->reply('/task_show - Отображение всех незавершенных заданий');
         $bot->reply('/task_add - Добавления задания в базу');
         $bot->reply('/task_finish {ID задания} - Завершить указаное задание');
         $bot->reply('/task_remove {ID задания} - Удалить указаное задание');
+        $bot->reply('/cinema_show - Отобразить расписание сеансов ТРК Галактика');
+    }
+
+    public function telegram($bot)
+    {
+        $bot->reply('Список доступных команд:' . PHP_EOL
+            . '=== Команды менеджера задач ===' . PHP_EOL
+            . '/task_show - Отображение всех незавершенных заданий' . PHP_EOL
+            . '/task_add - Добавления задания в базу' . PHP_EOL
+            . '/task_finish {ID задания} - Завершить указаное задание' . PHP_EOL
+            . '/task_remove {ID задания} - Удалить указаное задание' . PHP_EOL
+            . '=== Команды кинобота ===' . PHP_EOL
+            . '/cinema_show - Отобразить расписание сеансов ТРК Галактика' . PHP_EOL
+        );
     }
 }
--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,19 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('/start', 'App\Http\Controllers\Bot\StartController@index');
+$botman->group(['driver' => ['Telegram']], function($bot) {
+    $bot->hears('/start', 'App\Http\Controllers\Bot\StartController@telegram');
+});
+
+$botman->group(['driver' => ['Web']], function($bot) {
+    $bot->hears('/start', 'App\Http\Controllers\Bot\StartController@web');
+});
+
+$botman->group(['prefix' => 'test_'], function($bot) {
+    $bot->hears('1', function($bot) {
+        $bot->reply('Prefix response');
+    });
+});
 
 $botman->hears(
     '/task_show',
@@ -23,6 +35,11 @@ $botman->hears(
 $botman->hears(
     '/task_remove {taskId}',
     'App\Http\Controllers\Bot\TaskController@remove'
+);
+
+$botman->hears(
+    '/cinema_show',
+    'App\Http\Controllers\Bot\CinemaController@show'
 );
 
 $botman->fallback('App\Http\Controllers\Bot\FallbackController@index');
